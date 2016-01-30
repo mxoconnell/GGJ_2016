3d96b8740acccf7684bb61c88a5d42d730a4e2ed
ilookedinthebox <alex.kellner21@gmail.com>
2016-01-30 11:39:09 -0800

    Added view classes.

---
+++ Assets/CalendarView.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CalendarView : MonoBehaviour, IView
+{
+    public void ChangeView(IView nextView)
+    {
+        throw new NotImplementedException();
+    }
+
+    public RectTransform GetRoot()
+    {
+        return Root;
+    }
+
+    [SerializeField]
+    RectTransform Root;
+
+    [SerializeField]
+    Animator Controller;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GameLoop.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLoop : MonoBehaviour {
+
+	//CurrentView
+
+	// Use this for initialization
+	void Start () {
+		//Show splash screen
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
