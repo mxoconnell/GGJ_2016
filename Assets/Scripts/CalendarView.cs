4f15caecc854feacf530128c7eb3945676942f94
Matthew O'Connell <mxoconnell@gmail.com>
2016-01-31 15:07:59 -0800

    Calendar Logic begining

--- Assets/Scripts/CalendarView.cs
+++ Assets/Scripts/CalendarView.cs
@@ -7,13 +7,39 @@ using System;
 
 public class CalendarView : MonoBehaviour
 {
+    public GameObject popupMidGame;
+    public GameObject popupEndGame;
+    public GameObject[] days;
+    bool[] daysFilled = { true, false, false, false, false, false, false };//true if that day is filled
     [SerializeField]
     Image[] CalendarBookedGraphics;
 
     // Use this for initialization
     void Start() {
-
+        DisplayBookedDays();//initialize any pre-booked days
     }
+
+    void DisplayBookedDays()
+    {
+        int numberOfDates = 0;
+        for(int day = 0; day < daysFilled.Length; day++)
+        {
+            if (daysFilled[day])
+            {
+                numberOfDates++;
+                Debug.Log("day #" + day + " is booked!");
+                //add a heart if we have a date on that day
+            }
+        }
+
+        //All dates filled
+        if(numberOfDates == daysFilled.Length - 1)
+        {
+
+        }
+    }
+
+
 
     void SetCalendarGraphics()
     {
