b8ec68de50ab4765523c0f01b95b3dc2fe72917a
ilookedinthebox <alex.kellner21@gmail.com>
2016-01-30 17:27:02 -0800

    Calendar

--- Assets/Scripts/CalendarView.cs
+++ Assets/Scripts/CalendarView.cs
@@ -5,34 +5,21 @@ using System;
 
 //Buttons have OnClick methods tied via the editor to public methods
 
-public class CalendarView : MonoBehaviour, IView
+public class CalendarView : MonoBehaviour
 {
-    public void ChangeView(IView nextView)
-    {
-        throw new NotImplementedException();
-    }
-
-    public RectTransform GetRoot()
-    {
-        return Root;
-    }
-
-    [SerializeField]
-    RectTransform Root;
-
-    [SerializeField]
-    Animator Controller;
-
     [SerializeField]
     Image[] CalendarBookedGraphics;
 
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start() {
+
+    }
+
+    void SetCalendarGraphics()
+    {
+        for(int i = 0; i < CalendarBookedGraphics.Length; ++i)
+        {
+
+        }
+    }
 }
