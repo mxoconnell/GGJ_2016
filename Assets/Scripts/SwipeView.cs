d79a43dda8abca5f553ed8c3a93ec77ed3b6fc05
ilookedinthebox <alex.kellner21@gmail.com>
2016-01-31 14:22:16 -0800

    Swipeview fix

--- Assets/Scripts/SwipeView.cs
+++ Assets/Scripts/SwipeView.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEngine.UI;
 
 //Buttons have OnClick methods tied via the editor to public methods
 
@@ -8,6 +9,13 @@ public class SwipeView : MonoBehaviour, IView
 {
     public GameObject topCard;
     public GameObject bottomCard;
+
+    [SerializeField]
+    Image PortraitA, PortraitB;
+    [SerializeField]
+    Text NPCNameA, NPCNameB;
+    [SerializeField]
+    Text NPCAgeA, NPCAgeB;
 
     Vector3 topCardOriginalPosition;
     bool canSwipe = true; //false when we are swiping
@@ -19,6 +27,7 @@ public class SwipeView : MonoBehaviour, IView
     void Start()
     {
         topCardOriginalPosition = topCard.transform.position;
+        //initialize card graphics
     }
     public void ChangeView(IView nextView)
     {
@@ -92,8 +101,8 @@ public class SwipeView : MonoBehaviour, IView
                 //Now the top card and bottom card switch roles.
                 var placeholder = topCard;
                 topCard = bottomCard;
-                bottomCard = topCard;
-
+                bottomCard = placeholder;
+                FillNextPortrait();
             }
         }
     }
@@ -113,4 +122,14 @@ public class SwipeView : MonoBehaviour, IView
 
         }
     }
+
+    //Load in NPC Data
+
+    public void FillNextPortrait()
+    {
+        NPC newView = bottomCard.GetComponentInChildren<Image>().gameObject.AddComponent<NPC>();
+       // bottomCard.GetComponentInChildren<NPC>().InitializeNewNPC();
+        
+    }
+    
 }
